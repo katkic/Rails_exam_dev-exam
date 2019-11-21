fe6906fd25ec70701ad3506f6f8d0173fedfe697
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-22 00:43:41 +0900

    CSS, JavaScript, Helperファイルを、rails gで自動生成しない設定を追加

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,10 @@ module DevExam
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
