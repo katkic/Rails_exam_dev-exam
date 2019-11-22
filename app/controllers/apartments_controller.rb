db3d4dde2e8d219729a3a10fd031f1fac89dea32
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-22 15:20:14 +0900

    物件登録機能を実装

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -6,17 +6,39 @@ class ApartmentsController < ApplicationController
   end
 
   def new
+    @apartment = Apartment.new
+    2.times { @apartment.stations.build }
   end
 
   def edit
   end
 
   def create
+    @apartment = Apartment.new(apartment_params)
+
+    if @apartment.save
+      redirect_to @apartment, notice: '物件情報を登録しました'
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit(
+      :name,
+      :address,
+      :rent,
+      :year,
+      :remarks,
+      stations_attributes: %i[id route name walking_minutes]
+    )
   end
 end
