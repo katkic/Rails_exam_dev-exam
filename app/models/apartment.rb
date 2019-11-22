3c91b3f37d0a68392ec73244734f05a116ba4851
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-11-22 22:35:08 +0900

    バリデーションを追加

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,14 @@
 class Apartment < ApplicationRecord
   has_many :stations, dependent: :destroy
-  accepts_nested_attributes_for :stations
+  accepts_nested_attributes_for :stations,
+                                reject_if: proc { |attributes|
+                                  attributes[:route].blank? &&
+                                    attributes[:name].blank? &&
+                                    attributes[:walking_minutes].blank?
+                                }
+  validates :name, presence: true
+  validates :rent, presence: true, numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+  validates :address, presence: true
+  validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_blank: true }
+  validates :remarks, presence: true
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,7 @@
 class Station < ApplicationRecord
   belongs_to :apartment, optional: true
+
+  validates :route, presence: true
+  validates :name, presence: true
+  validates :walking_minutes, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_blank: true }
 end
